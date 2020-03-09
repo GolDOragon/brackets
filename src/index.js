ff8f52b54b64eff4e9dcbe4ac3afea47d8f179af
GolDOragon <56398970+GolDOragon@users.noreply.github.com>
2020-03-09 18:47:26 +0300

    feat: solve "Brackets" task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let order = [];
+
+    for (let i = 0; i < str.length; i++) {
+
+        let current_bracket = str[i];
+
+        
+        for (let j = 0; j < bracketsConfig.length; j++) {
+        
+            let open_bracket = bracketsConfig[j][0];
+            let close_bracket = bracketsConfig[j][1];
+
+            if (open_bracket != close_bracket) {
+        
+                if (current_bracket == open_bracket) {
+                    order.push(close_bracket);
+                } else if (current_bracket == close_bracket) {
+                    let order_last_bracket = order.pop();
+                    if (close_bracket != order_last_bracket) return false;
+                }
+        
+            } else {
+        
+                let order_last_bracket = order[order.length - 1];
+                if (
+                    current_bracket == open_bracket &&
+                    current_bracket != order_last_bracket
+                ) {
+                    order.push(close_bracket);
+                } else if (
+                    current_bracket == open_bracket &&
+                    current_bracket == order_last_bracket
+                ) {
+                    order.pop();
+                }
+            }
+        }
+    }
+    if (order.length != 0) return false;
+    return true;
+};
